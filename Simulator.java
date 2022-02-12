adad3e7aa4c1fb74b6eb020bb5e2284569ea4233
Salihah <sale7a1422@gmail.com>
2022-02-12 18:43:29 +0000

    Initial sharing of project

---
+++ Simulator.java
@@ -0,0 +1,191 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.awt.Color;
+
+/**
+ * A simple predator-prey simulator, based on a rectangular field
+ * containing rabbits and foxes.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public class Simulator
+{
+    // Constants representing configuration information for the simulation.
+    // The default width for the grid.
+    private static final int DEFAULT_WIDTH = 120;
+    // The default depth of the grid.
+    private static final int DEFAULT_DEPTH = 80;
+    // The probability that a fox will be created in any given grid position.
+    private static final double WOLF_CREATION_PROBABILITY = 0.02;
+    // The probability that a rabbit will be created in any given grid position.
+    private static final double SHEEP_CREATION_PROBABILITY = 0.08;  
+    
+    private static final double CAMEL_CREATION_PROBABILITY = 0.14; 
+    private static final double LION_CREATION_PROBABILITY = 0.10; 
+    private static final double TIGER_CREATION_PROBABILITY = 0.08; 
+
+
+    // List of animals in the field.
+    private List<Animal> animals;
+    // The current state of the field.
+    private Field field;
+    // The current step of the simulation.
+    private int step;
+    // A graphical view of the simulation.
+    private SimulatorView view;
+    
+    /**
+     * Construct a simulation field with default size.
+     */
+    public Simulator()
+    {
+        this(DEFAULT_DEPTH, DEFAULT_WIDTH);
+    }
+    
+    /**
+     * Create a simulation field with the given size.
+     * @param depth Depth of the field. Must be greater than zero.
+     * @param width Width of the field. Must be greater than zero.
+     */
+    public Simulator(int depth, int width)
+    {
+        if(width <= 0 || depth <= 0) {
+            System.out.println("The dimensions must be greater than zero.");
+            System.out.println("Using default values.");
+            depth = DEFAULT_DEPTH;
+            width = DEFAULT_WIDTH;
+        }
+        
+        animals = new ArrayList<>();
+        field = new Field(depth, width);
+
+        // Create a view of the state of each location in the field.
+        view = new SimulatorView(depth, width);
+        view.setColor(Sheep.class, Color.GRAY);
+        view.setColor(Wolf.class, Color.BLUE);
+        view.setColor(Camel.class, Color.RED);
+        view.setColor(Lion.class, Color.ORANGE);
+        view.setColor(Tiger.class, Color.BLACK);
+        
+        
+        // Setup a valid starting point.
+        reset();
+    }
+    
+    /**
+     * Run the simulation from its current state for a reasonably long period,
+     * (4000 steps).
+     */
+    public void runLongSimulation()
+    {
+        simulate(4000);
+    }
+    
+    /**
+     * Run the simulation from its current state for the given number of steps.
+     * Stop before the given number of steps if it ceases to be viable.
+     * @param numSteps The number of steps to run for.
+     */
+    public void simulate(int numSteps)
+    {
+        for(int step = 1; step <= numSteps && view.isViable(field); step++) {
+            simulateOneStep();
+            // delay(60);   // uncomment this to run more slowly
+        }
+    }
+    
+    /**
+     * Run the simulation from its current state for a single step.
+     * Iterate over the whole field updating the state of each
+     * fox and rabbit.
+     */
+    public void simulateOneStep()
+    {
+        step++;
+
+        // Provide space for newborn animals.
+        List<Animal> newAnimals = new ArrayList<>();        
+        // Let all rabbits act.
+        for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) {
+            Animal animal = it.next();
+            animal.act(newAnimals);
+            if(! animal.isAlive()) {
+                it.remove();
+            }
+        }
+               
+        // Add the newly born foxes and rabbits to the main lists.
+        animals.addAll(newAnimals);
+
+        view.showStatus(step, field);
+    }
+        
+    /**
+     * Reset the simulation to a starting position.
+     */
+    public void reset()
+    {
+        step = 0;
+        animals.clear();
+        populate();
+        
+        // Show the starting state in the view.
+        view.showStatus(step, field);
+    }
+    
+    /**
+     * Randomly populate the field with foxes and rabbits.
+     */
+    private void populate()
+    {
+        Random rand = Randomizer.getRandom();
+        field.clear();
+        for(int row = 0; row < field.getDepth(); row++) {
+            for(int col = 0; col < field.getWidth(); col++) {
+                if(rand.nextDouble() <= WOLF_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Wolf wolf = new Wolf(true, field, location);
+                    animals.add(wolf);
+                }
+                else if(rand.nextDouble() <= SHEEP_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Sheep sheep = new Sheep(true, field, location);
+                    animals.add(sheep);
+                }
+                else if(rand.nextDouble() <= CAMEL_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Camel camel = new Camel(true, field, location);
+                    animals.add(camel);
+                }
+                else if(rand.nextDouble() <= LION_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Lion lion = new Lion(true, field, location);
+                    animals.add(lion);
+                }
+                else if(rand.nextDouble() <= TIGER_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Tiger tiger = new Tiger(true, field, location);
+                    animals.add(tiger);
+                }
+                // else leave the location empty.
+            }
+        }
+    }
+    
+    /**
+     * Pause for a given time.
+     * @param millisec  The time to pause for, in milliseconds
+     */
+    private void delay(int millisec)
+    {
+        try {
+            Thread.sleep(millisec);
+        }
+        catch (InterruptedException ie) {
+            // wake up
+        }
+    }
+}
